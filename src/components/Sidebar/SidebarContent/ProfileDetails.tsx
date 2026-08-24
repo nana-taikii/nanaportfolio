38c4e1d27207429c21387da06680f2bfb20509b2
Donna Hope B. Banzuela <donna.banzuela@asialinkfinance.com.ph>
2026-08-24 09:26:26 +0800

    removed  unused imports

--- src/components/Sidebar/SidebarContent/ProfileDetails.tsx
+++ src/components/Sidebar/SidebarContent/ProfileDetails.tsx
@@ -2,7 +2,6 @@ import ImgProfile from "../../../assets/images/img_profile.png";
 import { ButtonGroup, Typography, Button, Link } from "@heroui/react";
 
 import IconSend from "../../../assets/svg/icon_send.svg";
-import IconFacebook from "../../../assets/svg/icon_facebook.svg";
 import IconLinkedin from "../../../assets/svg/icon_linkedin.svg";
 import IconGithub from "../../../assets/svg/icon_github.svg"
 
